f7023cc52dd84ac243df67d579b9490615692b8f
Kaitlyn Hova <kaitlynhova@gmail.com>
2022-09-18 08:01:25 -0700

    index

---
+++ pages/index.css.ts
@@ -0,0 +1,15 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "../theme.css";
+
+export const appStyles = style({
+  backgroundColor: vars.color.background,
+  padding: vars.spacing[44],
+});
+
+export const headerStyles = style({
+  paddingBottom: vars.spacing[44],
+});
+
+export const typographyWrapperStyles = style({
+  paddingBottom: vars.spacing[44],
+});
